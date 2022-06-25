50869a4611a13f5a7a5fefa956034c77df4e4856
wesleya2 <wesleyalvespereira@gmail.com>
2022-06-25 00:47:17 -0300

    frame pronto

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ class Application():
     def __init__(self):
         self.root = root
         self.tela()
+        self.frames_da_tela()
         root.mainloop()
 
     def tela(self):
         self.root.title('Cadastro de Clientes')
         self.root.configure(background='#1e3743')
-        #self.root.geometry('768x700')
+        self.root.geometry('768x700')
         self.root.resizable(True, True)
         self.root.maxsize(width=768, height=700)
 
     def frames_da_tela(self):
-        self.frame1 = Frame(self.root)
+        self.frame_1 = Frame(self.root, bd = 4, bg = '#dfe3ee', highlightbackground= '#759fe6', highlightthickness= '2')
+        self.frame_1.place(relx= 0.02, rely= 0.02, relwidth= 0.96, relheight= 0.46)
 
+        self.frame_2 = Frame(self.root, bd=4, bg='#dfe3ee', highlightbackground='#759fe6', highlightthickness='2')
+        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
 
 Application()
