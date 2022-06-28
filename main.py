40c4e3a4c557b33a437bb2904d14992c6c195f9b
wesleya2 <wesleyalvespereira@gmail.com>
2022-06-27 22:31:21 -0300

    layout ok

--- main.py
+++ main.py
@@ -25,24 +25,24 @@ class Application():
         self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
 
     def botoes_frame_1(self):
-        #Botao Limpar
-        self.bt_limpar = Button(self.frame_1, text='Limpar')
+        #Botao Limpar #bg cor botão, ufg cor do texto, font(tipo,tamanho,italigo/negrito) fg
+        self.bt_limpar = Button(self.frame_1, text='Limpar', border=2, bg = '#107db2', fg = 'white')
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
         # Botao Buscar
-        self.bt_buscar = Button(self.frame_1, text='Buscar')
+        self.bt_buscar = Button(self.frame_1, text='Buscar', border=2, bg = '#107db2', fg = 'white')
         self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
         # Botao Novo
-        self.bt_novo = Button(self.frame_1, text='Novo')
+        self.bt_novo = Button(self.frame_1, text='Novo', border=2, bg = '#107db2', fg = 'white')
         self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
         # Botao Alterar
-        self.bt_alterar = Button(self.frame_1, text='Alterar')
+        self.bt_alterar = Button(self.frame_1, text='Alterar', border=2, bg = '#107db2', fg = 'white')
         self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
         # Botao Apagar
-        self.bt_apagar = Button(self.frame_1, text='Apagar')
+        self.bt_apagar = Button(self.frame_1, text='Apagar', border=2, bg = '#107db2', fg = 'white')
         self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 
-        #Label Frame1/Input Código
-        self.lb_codigo = Label(self.frame_1, text = 'Código')
+        #Label Frame1/Input Código fg = '#107db2'
+        self.lb_codigo = Label(self.frame_1, text = 'Código', bg = '#dfe3ee')
         self.lb_codigo.place(relx=0.04, rely=0.05)
 
         self.codigo_entry = Entry(self.frame_1)
@@ -50,25 +50,25 @@ class Application():
 
         #Label Frame1/Input Nome
         self.lb_nome = Label(self.frame_1, text='Nome')
-        self.lb_nome.place(relx=0.04, rely=0.25)
+        self.lb_nome.place(relx=0.04, rely=0.33)
 
         self.nome_entry = Entry(self.frame_1)
-        self.nome_entry.place(relx=0.04, rely=0.30, relwidth=0.8, relheight=0.14)
+        self.nome_entry.place(relx=0.04, rely=0.43, relwidth=0.8, relheight=0.14)
 
         # Label Frame1/Input Telefone
         self.lb_telefone = Label(self.frame_1, text='Telefone')
-        self.lb_telefone.place(relx=0.04, rely=0.44)
+        self.lb_telefone.place(relx=0.04, rely=0.63)
 
         self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx=0.04, rely=0.49, relwidth=0.16, relheight=0.14)
+        self.telefone_entry.place(relx=0.04, rely=0.73, relwidth=0.16, relheight=0.14)
 
         #Label Frame1/Input Cidade
 
         self.lb_cidade = Label(self.frame_1, text='Cidade')
-        self.lb_cidade.place(relx=0.21, rely=0.44)
+        self.lb_cidade.place(relx=0.21, rely=0.63)
 
         self.cidade_entry = Entry(self.frame_1)
-        self.cidade_entry.place(relx=0.21, rely=0.49, relwidth=0.16, relheight=0.14)
+        self.cidade_entry.place(relx=0.21, rely=0.73, relwidth=0.16, relheight=0.14)
 
 
 Application()
